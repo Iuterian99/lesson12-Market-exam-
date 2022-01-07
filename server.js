97b51bbfc222653b43e09f7a42b9fc19bf862aea
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-08 01:33:35 +0500

    made nothing

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+const { branches } = require("./data/marketBranches.json");
+console.log(branches);
+
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url.substring(1) == "markets") {
@@ -20,6 +23,22 @@ const server = http.createServer((req, res) => {
 
       fs.readFile(
         path.resolve(__dirname, "./data/markets.json"),
+        (err, data) => {
+          if (err) throw err;
+          const foundMarkets = JSON.parse(data).filter(
+            (e) => e.id == marketsId
+          );
+          console.log(foundMarkets);
+        }
+      );
+      res.end("markets/1:/2");
+    }
+
+    if (req.url.substring(1, 11) == "marketInfo" && req.url.split("/")[2]) {
+      const marketsId = req.url.split("/")[2];
+
+      fs.readFile(
+        path.resolve(__dirname, "./data/marketBranches.json"),
         (err, data) => {
           if (err) throw err;
           const foundMarkets = JSON.parse(data).filter(
