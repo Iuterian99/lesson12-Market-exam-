0272c6a9c254fa792c39e22531786399b5b42fba
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-08 01:14:26 +0500

    did markets part

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const path = require("path");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
+    if (req.url.substring(1) == "markets") {
+      fs.readFile(
+        path.resolve(__dirname, "./data/markets.json"),
+        (err, data) => {
+          if (err) throw err;
+          console.log(JSON.parse(data));
+        }
+      );
+      res.end("marketlar:");
+    }
+
     if (req.url.substring(1, 8) == "markets" && req.url.split("/")[2]) {
       const marketsId = req.url.split("/")[2];
 
@@ -14,20 +25,11 @@ const server = http.createServer((req, res) => {
           const foundMarkets = JSON.parse(data).filter(
             (e) => e.id == marketsId
           );
-          console.log(JSON.parse(data));
           console.log(foundMarkets);
-
-          res.end("markets/1:/2");
         }
       );
-      res.end("markets:");
+      res.end("markets/1:/2");
     }
-    // if (req.url.substring(9) == "1") {
-    //   fs.readFile(path.resolve(__dirname, "./data/markets.js"), (err, data) => {
-    //     if (err) throw err;
-    //     console.log(data.toString());
-    //   });
-    // }
   }
 });
 
